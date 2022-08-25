c6e65ce18959c608d8ca2cbfd744c12236e2c67c
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2022-08-26 01:27:49 +0300

    Update http-common.js

--- react-hooks-crud-web-api/src/http-common.js
+++ react-hooks-crud-web-api/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://mern-deploy-backend-production.up.railway.app",
+  baseURL: "https://mern-deploy-backend-production.up.railway.app/api",
   headers: {
     "Content-type": "application/json"
   }
